f90574cf7f9c96eb2af7a0a1db2ae914760322de
justinconnolly <82484091+justinconnolly@users.noreply.github.com>
2021-07-14 23:28:02 -0400

    Update MontyHall.py

--- MontyHall.py
+++ MontyHall.py
@@ -10,42 +10,29 @@ def setTheStage():
 def revealDoor(doors: list, userSelection: int, winningSelection: int):
     numberedDoors = [0,1,2]
     if userSelection != winningSelection:
-        numberedDoors.remove(userSelection)
-        numberedDoors.remove(winningSelection)
-        return numberedDoors[0]
+        return 3 - userSelection - winningSelection
     else:
         numberedDoors.remove(userSelection)
         return numberedDoors[randint(0,1)]
-
-def doSwitch(doors: list, selection:int, openedDoor: int):
-    numberedDoors = [0,1,2]
-    numberedDoors.remove(selection)
-    numberedDoors.remove(openedDoor)
-    return numberedDoors[0]
     
 def MontyHall(switch: bool):
     stage = setTheStage()
-    #print(stage)
     userSelection = randint(0,2)
-    #print("User selection: " + str(userSelection))
-
     winningSelection = stage.index(True)
-    #print("Winning door: " + str(winningSelection))
     openedDoor = revealDoor(stage,userSelection,winningSelection)
-    #print("Revealed door: " + str(openedDoor))
-    switchedDoor = doSwitch(stage, userSelection, openedDoor)
-    #print("Switched to door: " + str(switchedDoor))
-    if switch and stage[switchedDoor] or stage[userSelection]:
-        #print("Winner!")
+    switchedDoor = 3 - userSelection - openedDoor
+    if switch and stage[switchedDoor]:
+        return 1
+    elif not switch and stage[userSelection]:
         return 1
     else:
-        #print("Loser!")
         return 0
         
 winners = 0
 numberOfGames = 100000
-switch = False
+switch = True
 for x in range(numberOfGames):
     winners += MontyHall(switch)
 winRate = winners/numberOfGames
-print("The overall winrate after {} games,{} taking the switch when offered, is {}".format(numberOfGames,"" if switch else " not",winRate))
+
+print("The overall winrate after {} games, {}taking the switch when offered, is {}.".format(numberOfGames, "" if switch else "not ",winRate))
