d44e960509dab8b31685c693d0853b8b89fecd8e
justinconnolly <82484091+justinconnolly@users.noreply.github.com>
2021-09-10 13:02:45 -0400

    Update cartesian-product-example.py

--- cartesian-product-example.py
+++ cartesian-product-example.py
@@ -1,9 +1,11 @@
+# Two lists of which we will get the Cartesian product (list1 X list2). Order matters, hence the use of "<>"
 list1 = [1,2,3]
 list2 = [4,5,6]
 
-
-
+# Iterate over list1
 for i in range(len(list1)):
+    
+    # Iterate over list2
     for j in range(len(list2)):
         print("<",end="")
         print(list1[i], end = ", ")                 # first number
