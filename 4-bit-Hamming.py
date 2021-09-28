d98273628bf36919c49495a75fc259500c5ac89d
justinconnolly <82484091+justinconnolly@users.noreply.github.com>
2021-09-28 19:57:54 -0400

    Update 4-bit-Hamming.py
    added automatically printing the 4 bit Hamming Code set

--- 4-bit-Hamming.py
+++ 4-bit-Hamming.py
@@ -1,14 +1,37 @@
 def getHamming(bits: list):
+    bits.append("-")
     bits.append(bits[1] ^ bits[2] ^ bits[3])
     bits.append(bits[0] ^ bits[2] ^ bits[3])
     bits.append(bits[0] ^ bits[1] ^ bits[3])
+    print(''.join([str(x) for x in bits]))
 
 if __name__ == '__main__':
-    while True:
-        userIn = input("Enter 4 bit code: ")
-        if userIn == "exit":
-            break
-        bitList = [int(x) for x in list(userIn)]
-        getHamming(bitList)
-        bitList.insert(4,'-')
-        print(''.join([str(x) for x in bitList]))
+    choice = ""
+    bin = ["0","1"]
+    while choice.lower() != "m" or choice.lower() != "a":
+        choice = input("(M)anual entry, (A)utomatic, or (Q)uit:\n")
+        if choice.lower() == "m":
+            while True:
+                userIn = input("Enter 4 bit binary code, (Q)uit, or return to the (M)enu:\n")
+                if userIn.lower() == "q":
+                    quit()
+                elif userIn.lower() == "m":
+                    break
+                charCheck = [character in bin for character in userIn]
+                if len(userIn) != 4 or not all(charCheck):
+                    continue
+                bitList = [int(x) for x in list(userIn)]
+                getHamming(bitList)
+                # print(''.join([str(x) for x in bitList]))
+                # print(bitList)
+        elif choice.lower() == "a":
+            from itertools import product
+
+            productList = []
+            products = list(product(bin,repeat=4))
+            for tuple in products:
+                productList.append([int(item) for item in tuple])
+                getHamming(productList[len(productList) - 1])
+                # print(''.join([str(x) for x in productList[len(productList) - 1]]))
+        elif choice.lower() == "q":
+            quit()
